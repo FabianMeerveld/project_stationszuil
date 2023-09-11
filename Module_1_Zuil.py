2f07c72d31dfc099c142dc52f125d738fc47998c
Fabian Meerveld <144360059+FabianMeerveld@users.noreply.github.com>
2023-09-11 23:29:32 +0200

    notities toegevoegd aan module 1

--- Module_1_Zuil.py
+++ Module_1_Zuil.py
@@ -1,7 +1,14 @@
+# Deze module vraagt op het station via de cli aan de gebruiker om een bericht en de naam, vervolgens kiest deze een
+# willekeurig station uit en wordt de huidige datum en tijd toegevoegd en wordt dit allemaal opgeslagen in een regel
+# van een bestand
+
+
 import datetime
 import random
 
 
+# functie die vraagt naar het bericht, controleert of deze korter is dan 140 tekens en of het bericht leeg is en
+# vervolgens het bericht terug geeft
 def get_message():
     while True:
         message = input("Laat hier uw bericht achter. (maximaal 140 tekens) \n")
@@ -15,6 +22,8 @@ def get_message():
             return message
 
 
+# functie die vraag naar de naam en deze terug geeft, als geen naam wordt ingevoerd dan wordt de naam anoniem terug
+# gegeven
 def get_name():
     name = input("Wat is uw naam? Laat dit veld leeg om anoniem te blijven. \n")
     if name == "":
@@ -22,11 +31,13 @@ def get_name():
     return name
 
 
+# functie die de huidige datum en tijd terug geeft
 def get_date():
     date = datetime.datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
     return date
 
 
+# functie die een willekeurige lijn uit een bestand met stations vraagt en deze terug geeft
 def get_station():
     station_file = open("Stations.txt", "r")
     station_list = station_file.readlines()
@@ -34,6 +45,7 @@ def get_station():
     return station
 
 
+# loop die constant de data opvraagt en (deze vervolgens weg stuurt om op te slaan | moet nog gemaakt worden)
 while True:
     data = get_message() + "," + get_name() + "," + get_date() + "," + get_station()
     print("Bedankt voor uw bericht!")
